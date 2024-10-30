1b6c3ca7c2a6913fc91f6befe3e8513f8149f4ee
Purplehollow117 <atlixqueno.barcenas.javier@gmail.com>
2024-10-29 23:11:01 -0600

    Geniaaal

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from flask import Flask, render_template
+
 app = Flask(__name__)
+
 @app.route('/')
 def home():
-    return render_template
+    return render_template('index.html')  # Asegúrate de que el nombre del archivo HTML sea correcto
+
 @app.route('/saludos')
-def saludos ():
-    return "Que onda!"
+def saludos():
+    return "¡Que onda!"
 
 if __name__ == '__main__':
     app.run(debug=True)
